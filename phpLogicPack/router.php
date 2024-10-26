e6889169337c4fbef155da8d95188359a94bafb6
unknown <your_email@example.com>
2024-10-26 14:46:44 -0700

    updated router file to allow dunamic middle ware handling

--- phpLogicPack/router.php
+++ phpLogicPack/router.php
@@ -3,16 +3,25 @@ class Router
 {
     private static $routes = [];
     private static $routeCounter = 0;
+    private static $groupStack = [];
+    private static $namedRoutes = [];
+    private static $fallbackHandler;
+    private static $middlewareGroups = [];
 
-    public static function addRoute($method, $url, $middleware)
+    public static function addRoute($method, $url, $handler, $middleware = null)
     {
+        $groupAttributes = self::getGroupAttributes();
+        $url = $groupAttributes['prefix'] . $url;
+        $middleware = array_merge($groupAttributes['middleware'], self::resolveMiddleware($middleware));
+
         $routeId = 'route_' . self::$routeCounter++;
         self::$routes[$routeId] = [
             'method' => strtoupper($method),
             'url' => $url,
+            'handler' => $handler,
             'middleware' => $middleware
         ];
-        return $routeId;
+        return new Route($routeId);
     }
 
     public static function handleRequest()
@@ -20,49 +29,57 @@ class Router
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['REQUEST_URI'];
 
-        $matchedPath = false;
-
         foreach (self::$routes as $route) {
             $matches = self::match($path, $route['url']);
-            if ($matches !== false) {
-                $matchedPath = true;
-                if ($route['method'] === $method) {
-                    $queryParams = [];
-                    $queryString = parse_url($path, PHP_URL_QUERY);
-                    if ($queryString !== null) {
-                        parse_str($queryString, $queryParams);
-                    }
-                    $allParams = array_merge($matches, $queryParams);
-                    
-                    switch ($method) {
-                        case 'POST':
-                            $requestData = $_POST;
-                            break;
-                        case 'GET':
-                            $requestData = $_GET;
-                            break;
-                        case 'PUT':
-                        case 'DELETE':
-                            $input = file_get_contents("php://input");
-                            if (!empty($input)) {
-                                parse_str($input, $requestData);
-                            } else {
-                                $requestData = [];
-                            }
-                            break;
-                        default:
-                            $requestData = [];
-                    }
-                    
-                    return $route['middleware']($allParams, $matches, $requestData);
+            if ($matches !== false && $route['method'] === $method) {
+                $queryParams = [];
+                $queryString = parse_url($path, PHP_URL_QUERY);
+                if ($queryString !== null) {
+                    parse_str($queryString, $queryParams);
                 }
+                $allParams = array_merge($matches, $queryParams);
+
+                // Execute middleware if it exists
+                foreach ($route['middleware'] as $middleware) {
+                    $middlewareResult = $middleware($allParams, $matches);
+                    if ($middlewareResult === false) {
+                        // If middleware returns false, stop execution
+                        return;
+                    }
+                }
+
+                // Execute the main handler
+                switch ($method) {
+                    case 'POST':
+                        $requestData = $_POST;
+                        break;
+                    case 'GET':
+                        $requestData = $_GET;
+                        break;
+                    case 'PUT':
+                    case 'DELETE':
+                        $input = file_get_contents("php://input");
+                        if (!empty($input)) {
+                            parse_str($input, $requestData);
+                        } else {
+                            $requestData = [];
+                        }
+                        break;
+                    default:
+                        $requestData = [];
+                }
+
+                $response = $route['handler']($allParams, $matches, $requestData);
+                self::sendResponse(200, $response);
+                return;
             }
         }
 
-        if ($matchedPath) {
-            self::sendResponse(405, 'Method Not Allowed');
+        if (self::$fallbackHandler) {
+            $response = call_user_func(self::$fallbackHandler);
+            self::sendResponse(404, $response);
         } else {
-            self::sendResponse(404, 'Path Not Found');
+            self::sendResponse(404, 'Not Found');
         }
     }
 
@@ -72,53 +89,111 @@ class Router
         $urlParts = parse_url($url);
         $urlPath = $urlParts['path'];
 
-        // Convert the URL path into a regex pattern for matching named parameters
         $urlPattern = preg_replace_callback('/\{(\w+)\}/', function ($matches) {
             return '(?P<' . $matches[1] . '>[^/]+)';
         }, $urlPath);
 
-        // Escape forward slashes and add start/end anchors
         $urlPattern = '/^' . str_replace('/', '\/', $urlPattern) . '$/';
 
         if (preg_match($urlPattern, $path, $matches)) {
-            $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
-
-            // If there's a query string in the URL, add it to the params
-            if (isset($urlParts['query'])) {
-                parse_str($urlParts['query'], $queryParams);
-                $params = array_merge($params, $queryParams);
-            }
-
-            return $params;
+            return array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
         }
 
         return false;
     }
 
-    public static function post($url, $middleware)
+    public static function get($url, $handler, $middleware = null)
     {
-        return self::addRoute('POST', $url, $middleware);
+        return self::addRoute('GET', $url, $handler, $middleware);
     }
 
-    public static function get($url, $middleware)
+    public static function post($url, $handler, $middleware = null)
     {
-        return self::addRoute('GET', $url, $middleware);
+        return self::addRoute('POST', $url, $handler, $middleware);
     }
 
-    public static function put($url, $middleware)
+    public static function put($url, $handler, $middleware = null)
     {
-        return self::addRoute('PUT', $url, $middleware);
+        return self::addRoute('PUT', $url, $handler, $middleware);
     }
 
-    public static function delete($url, $middleware)
+    public static function delete($url, $handler, $middleware = null)
     {
-        return self::addRoute('DELETE', $url, $middleware);
+        return self::addRoute('DELETE', $url, $handler, $middleware);
     }
 
     private static function sendResponse($statusCode, $message)
     {
-        //http_response_code($statusCode);
+        http_response_code($statusCode);
         echo json_encode(['status' => $statusCode, 'message' => $message]);
         exit;
+    }
+
+    public static function group($attributes, $callback)
+    {
+        self::$groupStack[] = $attributes;
+        call_user_func($callback);
+        array_pop(self::$groupStack);
+    }
+
+    private static function getGroupAttributes()
+    {
+        $prefix = '';
+        $middleware = [];
+        foreach (self::$groupStack as $group) {
+            if (isset($group['prefix'])) {
+                $prefix .= '/' . trim($group['prefix'], '/');
+            }
+            if (isset($group['middleware'])) {
+                $middleware = array_merge($middleware, (array)$group['middleware']);
+            }
+        }
+        return ['prefix' => $prefix, 'middleware' => $middleware];
+    }
+
+    public static function url($name, $parameters = [])
+    {
+        if (!isset(self::$namedRoutes[$name])) {
+            throw new Exception("Route not found: $name");
+        }
+        $url = self::$routes[self::$namedRoutes[$name]]['url'];
+        foreach ($parameters as $key => $value) {
+            $url = str_replace("{{$key}}", $value, $url);
+        }
+        return $url;
+    }
+
+    public static function fallback($handler)
+    {
+        self::$fallbackHandler = $handler;
+    }
+
+    public static function middlewareGroup($name, array $middleware)
+    {
+        self::$middlewareGroups[$name] = $middleware;
+    }
+
+    private static function resolveMiddleware($middleware)
+    {
+        if (is_string($middleware) && isset(self::$middlewareGroups[$middleware])) {
+            return self::$middlewareGroups[$middleware];
+        }
+        return (array)$middleware;
+    }
+}
+
+class Route
+{
+    private $routeId;
+
+    public function __construct($routeId)
+    {
+        $this->routeId = $routeId;
+    }
+
+    public function name($name)
+    {
+        Router::$namedRoutes[$name] = $this->routeId;
+        return $this;
     }
 }
